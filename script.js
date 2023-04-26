c58e4374c10b428635ae1f6ec03361f3b2a6599b
BrunoDeSP <antunesbrunobarbosa1@gmail.com>
2023-04-26 17:12:11 -0300

    logic insert completed picture

--- script.js
+++ script.js
@@ -1,33 +1,76 @@
 let turn = "circle"
 let quadrado = ""
-console.log(turn)
+const quadrados = Array.prototype.slice.call(document.getElementsByClassName('square'))  
+const lista = [
+    quadradoUm = {
+        fundo : ""
+    },
+    quadradoDois = {
+        fundo : ""
+    },
+    quadradoTres = {
+        fundo : ""
+    },
+    quadradoQuatro = {
+        fundo : ""
+    },
+    quadradoCinco = {
+        fundo : ""
+    },
+    quadradoSeis = {
+        fundo : ""
+    },
+    quadradoSete = {
+        fundo : ""
+    },
+    quadradoOito = {
+        fundo : ""
+    },
+    quadradoNove = {
+        fundo : ""
+    },
+]
 
 function trocar(name) {
-    const figure = document.getElementsByClassName(`square--${name}`)
-    // const resolucao = quadrado == figure[0] ? 'true' : 'false'
-    // quadrado = figure[0]
-    // console.log(resolucao)
-    if (quadrado == figure[0]) {
+    const figure = Array.prototype.slice.call(document.getElementsByClassName(`square--${name}`))
+    const position = quadrados.indexOf(figure[0])
+    const verificacao = quadrados[position].className == `square square--${name}` ? true : false
+    const ultimaForma = figure[0] == quadrado ? true : false
+    
+    if(ultimaForma) {
         if(turn == "circle") {
+            figure[0].classList.remove("activeCrucifix")
             turn = "crucifix"
-            quadrado.classList.remove("activeCrucifix")
-            quadrado = ""    
-        } else { 
-            turn = "circle"
-            quadrado.classList.remove("activeCircle")
-            quadrado = ""
-        }
-    } else {
-        if (turn == "circle") {
-            figure[0].classList.add("activeCircle")
-            turn = "crucifix"
-            quadrado = figure[0]
         } else {
-            figure[0].classList.add("activeCrucifix")
-            turn = "circle"  
-            quadrado = figure[0]    
+            figure[0].classList.remove("activeCircle")
+            turn = "circle"
+        }
+        quadrado = ""
+        } else {
+            if(verificacao) {
+                if (turn == "circle") {
+                    figure[0].classList.add("activeCircle")
+                    turn = "crucifix"
+                    quadrado = figure[0]
+                } else {
+                    figure[0].classList.add("activeCrucifix")
+                    turn = "circle"  
+                    quadrado = figure[0]    
+            }    
         }
     }
-    
-    console.log(turn)
 }
+
+function reiniciar() {
+    for(i = 0; i < 9; i++) {
+        quadrados[i].classList.remove("activeCrucifix")
+        quadrados[i].classList.remove("activeCircle")
+    }    
+}
+
+
+
+
+
+
+
