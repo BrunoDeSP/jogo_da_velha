7ae7a24abd171d3e6c4f4ed494a0b37df014c820
BrunoDeSP <antunesbrunobarbosa1@gmail.com>
2023-05-02 18:28:19 -0300

    data being inserted into the array

--- script.js
+++ script.js
@@ -3,30 +3,39 @@ let quadrado = ""
 const quadrados = Array.prototype.slice.call(document.getElementsByClassName('square'))  
 const lista = [
     quadradoUm = {
+        nome: "one",
         fundo : ""
     },
     quadradoDois = {
+        nome: "two",
         fundo : ""
     },
     quadradoTres = {
+        nome: "three",
         fundo : ""
     },
     quadradoQuatro = {
+        nome: "four",
         fundo : ""
     },
     quadradoCinco = {
+        nome: "five",
         fundo : ""
     },
     quadradoSeis = {
+        nome: "six",
         fundo : ""
     },
     quadradoSete = {
+        nome: "seven",
         fundo : ""
     },
     quadradoOito = {
+        nome: "eight",
         fundo : ""
     },
     quadradoNove = {
+        nome: "nine",
         fundo : ""
     },
 ]
@@ -36,14 +45,17 @@ function trocar(name) {
     const position = quadrados.indexOf(figure[0])
     const verificacao = quadrados[position].className == `square square--${name}` ? true : false
     const ultimaForma = figure[0] == quadrado ? true : false
-    
+    // const inserirForma = lista.find( item => item.nome == '')
+
     if(ultimaForma) {
         if(turn == "circle") {
             figure[0].classList.remove("activeCrucifix")
             turn = "crucifix"
+            retirarForma(name)
         } else {
             figure[0].classList.remove("activeCircle")
             turn = "circle"
+            retirarForma(name)
         }
         quadrado = ""
         } else {
@@ -52,10 +64,12 @@ function trocar(name) {
                     figure[0].classList.add("activeCircle")
                     turn = "crucifix"
                     quadrado = figure[0]
+                    inserirForma(name, "circle")                
                 } else {
                     figure[0].classList.add("activeCrucifix")
                     turn = "circle"  
-                    quadrado = figure[0]    
+                    quadrado = figure[0]                       
+                    inserirForma(name, "crucifix")                
             }    
         }
     }
@@ -68,7 +82,20 @@ function reiniciar() {
     }    
 }
 
+// function acharGanhador() {
+    
+// }
 
+function inserirForma(name, figure) {
+    const inserindo = lista.find(item => item.nome == name)
+    inserindo.fundo = figure    
+}
+
+function retirarForma(name) {
+    const retirando = lista.find(item => item.nome == name)
+    retirando.fundo = ""
+}
+// console.log(arrayCrucifixo)
 
 
 
